9ed759d4c3b8cd8f6be2e28b0c99a70657b90134
raxemmohamed <raxemmohamed@gmail.com>
2019-07-18 19:10:24 -0500

    commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 const Table = require('cli-table3');
 
 const db = mysql.createConnection({
-    host: "localhost", 
+    host: "localhost",
     port: "3306",
     user: "root",
     password: "password",
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
 });
 
 db.connect(function (err) {
-    if(err) throw err;
+    if (err) throw err;
     console.log("connected as id : ", db.threadId);
     showItems();
 });
@@ -22,11 +22,58 @@ function showItems() {
             head: ["Item ID", "Product Name", "Department Name", "Price", "Quantity"],
             colWidths: [10, 30, 30, 10, 10]
 
+        });
+        for (let i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].quantity]
+            );
+        }
+        console.log(table.toString());
+
+        setTimeout(purchase, 1000);
     });
-    for (let i = 0; i < res.length; i++) {
-        table.push(
-            [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].quantity]
-        );
-    }
-    console.log()
+};
+
+function purchase() {
+    inquirer.prompt([
+        {
+            name: "item_id",
+            type: "input",
+            message: "Please select item id."
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many would you like?"
+        }
+    ]).then(function (input) {
+        db.query("SELECT * FROM products WHERE item_id = ?", input.item_id, function (err, res) {
+            if(err) throw err;
+
+            if(!res.length) {
+                console.log("Sorry, we do not have that item in our inventory.  Please try again");
+                for (let i = 0; i < 5; i++) {
+                    console.log("\r\n");
+                };
+                return purchase();
+            };
+
+            if(parseInt(input.quantity) > parseInt(res[0].stock_quantity)) {
+                console.log("Sorry, we do not have that amount at this time.  Please try again.");
+                return purchase(); 
+            };
+
+            var newQuantity = parseInt(res[0].stock_quantity) - parseInt(input.quantity);
+            var subTotal = parseFloat(res[0].price) * parseInt(input.quantity);
+
+            db.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [ newQuantity, input.item_id ], function (err, res) {
+                if (err) throw err;
+
+                console.log(`Your total is $${subTotal}.  Thank you for your purchase!`);
+
+                db.end();
+                
+            })
+        })
+    })
 }
